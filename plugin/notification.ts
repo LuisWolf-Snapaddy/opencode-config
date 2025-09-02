a1f286dd87f985aa645b4b3f697037d4f3506f4b
pawcode Development <development@pawcode.de>
2025-09-02 22:26:31 +0200

    feat(plugin): update notification plugin to include repository name

--- plugin/notification.ts
+++ plugin/notification.ts
@@ -1,10 +1,14 @@
 import type { Plugin } from '@opencode-ai/plugin';
 
-export const NotificationPlugin: Plugin = async ({ app, client, $ }) => {
+export const NotificationPlugin: Plugin = async ({ $, worktree }) => {
+  const gitIndex = worktree.split('/').indexOf('.git');
+  const repositoryName = worktree.split('/').at(gitIndex - 1);
+
   return {
     event: async ({ event }) => {
       if (event.type === 'session.idle') {
-        await $`notify-send "opencode agent completed their task"`;
+        const notification = `opencode agent completed their task in repository ${repositoryName}`;
+        await $`notify-send "${notification}"`;
       }
     },
   };
